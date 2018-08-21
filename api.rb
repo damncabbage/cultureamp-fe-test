60840f92f0f59fd2edeae8aa99ce9562a2071f95
Rob Howard <rob@robhoward.id.au>
2018-08-22 00:47:25 +1000

    Fix weird tabbing issues; oops.

--- api.rb
+++ api.rb
@@ -1,21 +1,21 @@
 require 'sinatra'
 
 class SurveyAPI < Sinatra::Base
-	set :data_folder, File.expand_path('data', __dir__)
+  set :data_folder, File.expand_path('data', __dir__)
 
   get '/', provides: 'json' do
     send_file File.expand_path(
-			'index.json',
-		  settings.data_folder
-		)
+      'index.json',
+      settings.data_folder
+    )
   end
 
   get '/survey_results/:id(.json)?', provides: 'json' do |id_string|
     id = Integer(id_string) rescue halt(400)
     send_file File.expand_path(
       "#{id}.json",
-			File.join(settings.data_folder, 'survey_results')
-		)
+      File.join(settings.data_folder, 'survey_results')
+    )
   end
 
   def error_json(msg)
